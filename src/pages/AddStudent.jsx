4916473752b92d3f3ec0ee2b441167d558d3ae9a
Nehru raja <nehruraja9485@gmail.com>
2026-03-24 13:32:34 +0530

    Day 2

--- src/pages/AddStudent.jsx
+++ src/pages/AddStudent.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
 import Nav from '../components/Nav'
 import { useState } from 'react'
+import axios from 'axios'
+import { toast,ToastContainer } from 'react-toastify'
 const AddStudent = () => {
   const [name,setName] = useState("")
   const [email,setEmail] = useState("")
   const [department,setDepartment] = useState("")
   const [course, setCourse] = useState("")
+
+  function saveData(e){
+      e.preventDefault()
+      const userData = {name,email,department,course}
+      axios.post("http://localhost:3000/users",userData)
+      .then(()=>{
+        toast.success("Student Added")
+        setName("")
+        setEmail("")
+        setDepartment("")
+        setCourse("")
+      })
+      .catch(err=>console.log(err))
+  }
   return (
     <>
       <Nav/>
+      <ToastContainer/>
       <center>
         <h1>Add Student</h1>
-
-        <form action="">
+        <form action="" onSubmit={saveData}>
           <input 
           type="text" 
           placeholder='Enter student name' required value={name} 
